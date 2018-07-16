780323612b82f928b6def49ac4782b43d21835f0
HenG <hello@hen.zone>
2018-07-16 15:56:54 +0800

    Add `to` and `withRouter` to button

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
       1,
       "always"
     ],
-    "react/default-props-match-prop-types": 0
+    "react/default-props-match-prop-types": 0,
+    "jsx-a11y/anchor-is-valid": [ "error", {
+      "components": [ "Link" ],
+      "specialLink": [ "to" ],
+    }]
   }
 };
   
