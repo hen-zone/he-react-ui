589d647814e9b02d81c2641d669298981c38f9b0
HenG <hello@hen.zone>
2018-07-20 12:51:43 +0800

    Fix clicking outside and overfow issues

--- src/components/Tutorial/TutorialStep.js
+++ src/components/Tutorial/TutorialStep.js
@@ -2,12 +2,13 @@
 
 import classnames from 'classnames';
 import * as React from 'react';
+import onClickOutside from 'react-onclickoutside';
 import Icon from '../Icon';
 import CarouselIndicator from '../Layout/CarouselIndicator';
 import FadeIn from './FadeIn';
+import getCoordsForElementId from './getCoordsForElementId';
 import styles from './Tutorial.scss';
 import withTutorial from './withTutorial';
-import getCoordsForElementId from './getCoordsForElementId';
 
 function everyFrame(handler) {
   let stopped = false;
@@ -40,142 +41,157 @@ type Props = {
   attachTo?: string,
 };
 
+const cancelClick = (ev: MouseEvent) => {
+  ev.stopPropagation();
+};
 export default withTutorial(
-  class TutorialStep extends React.Component<Props, *> {
-    static defaultProps = {
-      showCarousel: false,
-      children: null,
-      className: null,
-      centered: false,
-    };
-
-    state = {
-      ownDomElement: null,
-      top: 100,
-      left: 76,
-      reversed: false,
-      isAttached: false,
-    };
-
-    componentWillMount() {
-      this.pollAttachedElement();
-      this.stopRepeating = everyFrame(this.pollAttachedElement);
-    }
-
-    componentWillUnmount() {
-      if (this.stopRepeating) this.stopRepeating();
-    }
-
-    receiveOwnDomElement = ownDomElement => {
-      if (ownDomElement !== this.state.ownDomElement) {
-        this.setState({ ownDomElement });
-      }
-    };
-
-    pollAttachedElement = () => {
-      const { attachTo } = this.props;
-
-      if (!attachTo) return;
-
-      const coords = getCoordsForElementId(attachTo);
-
-      if (coords) {
-        const top = coords.top + coords.height / 2;
-        const left = coords.right;
-        const reversed = coords.top > window.innerHeight / 2;
-
-        if (
-          top !== this.state.top ||
-          left !== this.state.left ||
-          reversed !== this.state.reversed
-        ) {
-          this.setState({ top, left, reversed, isAttached: true });
-        }
-      }
-    };
-
-    stopRepeating = null;
-
-    render() {
-      const {
-        onTutorialAdvance,
-        onTutorialDismiss,
-        tutorialIndex,
-        tutorialSteps,
-        id,
-        showCarousel,
-        children,
-        className,
-        centered,
-      } = this.props;
-
-      const { top, left, reversed, isAttached, ownDomElement } = this.state;
-
-      const currentStep = tutorialSteps[tutorialIndex];
-
-      if (currentStep !== id) {
-        return null;
-      }
-
-      const popupClasses = classnames(styles.popup, className, {
-        [styles.popupCentered]: centered,
-      });
-      let arrowStyle = {};
-      let newTop = top - 75;
-
-      if (ownDomElement && reversed) {
-        newTop -= ownDomElement.getBoundingClientRect().height - 150;
-        arrowStyle = {
-          top: ownDomElement.getBoundingClientRect().height - 75,
-        };
-      }
-      const wrapperStyle = {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        top: newTop,
-        left,
-        right: 'auto',
+  onClickOutside(
+    class TutorialStep extends React.Component<Props, *> {
+      static defaultProps = {
+        showCarousel: false,
+        children: null,
+        className: null,
+        centered: false,
       };
 
-      return (
-        <div className={styles.outer} style={wrapperStyle}>
-          <FadeIn>
-            <div className={popupClasses} ref={this.receiveOwnDomElement}>
-              <Icon
-                className={styles.close}
-                name="Cross"
-                onClick={onTutorialDismiss}
-              />
+      state = {
+        ownDomElement: null,
+        top: 100,
+        left: 76,
+        reversed: false,
+        isAttached: false,
+      };
 
-              <div className={styles.tutorialWrapper}>
-                {children}
+      componentWillMount() {
+        this.pollAttachedElement();
+        this.stopRepeating = everyFrame(this.pollAttachedElement);
+      }
 
-                {showCarousel && (
-                  <CarouselIndicator
-                    className={styles.tutorialIndicator}
-                    length={tutorialSteps.length}
-                    current={tutorialIndex}
-                    nextStep={onTutorialAdvance}
-                  />
-                )}
-              </div>
-            </div>
-          </FadeIn>
+      componentWillUnmount() {
+        if (this.stopRepeating) this.stopRepeating();
+      }
 
-          {isAttached && (
+      handleClickOutside = () => {
+        this.props.onTutorialDismiss();
+      };
+
+      receiveOwnDomElement = ownDomElement => {
+        if (ownDomElement !== this.state.ownDomElement) {
+          this.setState({ ownDomElement });
+        }
+      };
+
+      pollAttachedElement = () => {
+        const { attachTo } = this.props;
+
+        if (!attachTo) return;
+
+        const coords = getCoordsForElementId(attachTo);
+
+        if (coords) {
+          const top = coords.top + coords.height / 2;
+          const left = coords.right;
+          const reversed = coords.top > window.innerHeight / 2;
+
+          if (
+            top !== this.state.top ||
+            left !== this.state.left ||
+            reversed !== this.state.reversed
+          ) {
+            this.setState({ top, left, reversed, isAttached: true });
+          }
+        }
+      };
+
+      stopRepeating = null;
+
+      render() {
+        const {
+          onTutorialAdvance,
+          onTutorialDismiss,
+          tutorialIndex,
+          tutorialSteps,
+          id,
+          showCarousel,
+          children,
+          className,
+          centered,
+        } = this.props;
+
+        const { top, left, reversed, isAttached, ownDomElement } = this.state;
+
+        const currentStep = tutorialSteps[tutorialIndex];
+
+        if (currentStep !== id) {
+          return null;
+        }
+
+        const popupClasses = classnames(styles.popup, className, {
+          [styles.popupCentered]: centered,
+        });
+        let arrowStyle = {};
+        let newTop = top - 75;
+
+        if (ownDomElement && reversed) {
+          newTop -= ownDomElement.getBoundingClientRect().height - 150;
+          arrowStyle = {
+            top: ownDomElement.getBoundingClientRect().height - 75,
+          };
+        }
+        const wrapperStyle = {
+          position: 'absolute',
+          top: newTop,
+          left,
+          right: 'auto',
+        };
+
+        return (
+          <div
+            className={styles.outer}
+            style={wrapperStyle}
+            onClick={this.props.onTutorialDismiss}
+          >
             <FadeIn>
-              <Icon
-                className={classnames(styles.arrow, {
-                  [styles.reversed]: reversed,
-                })}
-                style={arrowStyle}
-                name="CurvedArrow"
-              />
+              <div
+                className={popupClasses}
+                ref={this.receiveOwnDomElement}
+                onClick={cancelClick}
+              >
+                <Icon
+                  className={styles.close}
+                  name="Cross"
+                  onClick={onTutorialDismiss}
+                />
+
+                <div className={styles.tutorialWrapper}>
+                  {children}
+
+                  {showCarousel && (
+                    <CarouselIndicator
+                      className={styles.tutorialIndicator}
+                      length={tutorialSteps.length}
+                      current={tutorialIndex}
+                      nextStep={onTutorialAdvance}
+                    />
+                  )}
+                </div>
+              </div>
             </FadeIn>
-          )}
-        </div>
-      );
-    }
-  },
+
+            {isAttached && (
+              <FadeIn>
+                <Icon
+                  className={classnames(styles.arrow, {
+                    [styles.reversed]: reversed,
+                  })}
+                  style={arrowStyle}
+                  name="CurvedArrow"
+                />
+              </FadeIn>
+            )}
+          </div>
+        );
+      }
+    },
+  ),
 );
